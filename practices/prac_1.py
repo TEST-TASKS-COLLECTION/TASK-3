41d4e251570b686bdd179ca1d334b04f40e01963
mikeyy <adhikari.shrish1@gmail.com>
2022-09-06 13:35:05 +0545

    Checked another program

--- practices/prac_1.py
+++ practices/prac_1.py
@@ -4,12 +4,14 @@ async def main():
     task = asyncio.create_task(other_func())
     print("A")
     await asyncio.sleep(1)
-    await asyncio.sleep(3)
     print("B")
+    msg = await task
+    print(msg)
     
 async def other_func():
     print("1")
     await asyncio.sleep(2)
     print("2")
+    return "HELLO"
 
 asyncio.run(main())

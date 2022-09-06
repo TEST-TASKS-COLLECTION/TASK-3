a3428bd863c409a02139040f36a44d667cc60362
mikeyy <adhikari.shrish1@gmail.com>
2022-09-06 23:09:12 +0545

    Mocking a url fetch

---
+++ tasks/task_1.py
@@ -0,0 +1,21 @@
+import asyncio
+
+async def fetch(url):
+    print(f"Fetching {url}")
+    await asyncio.sleep(1)
+    return {
+        "url": url
+    }
+
+async def main():
+    data = await asyncio.gather(
+        fetch("1st url"),
+        fetch("2nd url"),
+        fetch("3rd url"),
+        fetch("4th url"),
+        fetch("5th url"),
+    )
+
+    print(data)
+
+asyncio.run(main()) 
